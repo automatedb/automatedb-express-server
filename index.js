dfa7e16a8d5a2cb731c8a03585f8426506763657
automatedb <contact@robots-trading.com>
2017-06-02 19:22:29 +0200

    Ajout du Controller dans les options

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Server = require('./app/libs/Server');
 const Response = require('./app/libs/Response');
+const Controller = require('./app/libs/Controller');
 
 module.exports = {
     Server,
-    Response
+    Response,
+    Controller
 };
